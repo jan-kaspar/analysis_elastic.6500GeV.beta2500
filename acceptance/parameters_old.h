5d81aa9deb6d5deb7c5498e831cb7e29574c3f12
Jan Kašpar <jan.kaspar@cern.ch>
2017-02-13 16:28:28 +0100

    Validation script: comparison of old and new acceptance calculation.

---
+++ acceptance/parameters_old.h
@@ -0,0 +1,64 @@
+#include "parameters_global.h"
+
+double timestamp_min = 1.5E3, timestamp_max = 32.0E3;
+
+void Init_base()
+{
+	// load global settings
+	Init_global();
+
+	// selection of bunches
+	keepAllBunches = true;
+	//bunchMap[94882].push_back(0);
+
+	// approximate (time independent) resolutions
+	anal.si_th_y_1arm = 0.37E-6 / sqrt(2.);
+
+	anal.si_th_x_LRdiff = 12.5E-6;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45b_56t()
+{
+	Init_global_45b_56t();
+
+	// analysis settings
+	anal.cut1_a = 1.; anal.cut1_c = 0.4E-6; anal.cut1_si = 3E-6;
+	anal.cut2_a = 1.; anal.cut2_c = -0.02E-6; anal.cut2_si = 0.5E-6;
+	
+	anal.cut7_a = 0.; anal.cut7_c = -0.007; anal.cut7_si = 0.3;
+
+	// fiducial cuts
+	anal.fc_L_l = FiducialCut(4.1E-6, 0E-6, 0., 0E-6, 0.);
+	anal.fc_L_h = FiducialCut(102E-6, 0E-6, 0., 0E-6, 0.);
+
+	anal.fc_R_l = FiducialCut(4.2E-6, 0E-6, 0., 0E-6, 0.);
+	anal.fc_R_h = FiducialCut(102E-6, 0E-6, 0., 0E-6, 0.);
+
+	anal.fc_G_l = FiducialCut(4.3E-6, 0E-6, 0., 0E-6, 0.);
+	anal.fc_G_h = FiducialCut(100E-6, 0E-6, 0., 0E-6, 0.);
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45t_56b()
+{
+	Init_global_45t_56b();
+
+	// analysis settings
+	anal.cut1_a = 1.; anal.cut1_c = -0.2E-6; anal.cut1_si = 3.0E-6;
+	anal.cut2_a = 1.; anal.cut2_c = +0.01E-6; anal.cut2_si = 0.5E-6;
+	
+	anal.cut7_a = 0.; anal.cut7_c = 0.02; anal.cut7_si = 0.3;
+
+	// fiducial cuts
+	anal.fc_L_l = FiducialCut(4.1E-6, 0E-6, 0., 0E-6, 0.);
+	anal.fc_L_h = FiducialCut(102E-6, 0E-6, 0., 0E-6, 0.);
+
+	anal.fc_R_l = FiducialCut(4.1E-6, 0E-6, 0., 5E-6, 0.);
+	anal.fc_R_h = FiducialCut(102E-6, 0E-6, 0., 0E-6, 0.);
+
+	anal.fc_G_l = FiducialCut(4.2E-6, 2E-6, 0., 5E-6, 0.);
+	anal.fc_G_h = FiducialCut(100E-6, 0E-6, 0., 0E-6, 0.);
+}
--- acceptance/validation_vs_old.asy
+++ acceptance/validation_vs_old.asy
@@ -7,16 +7,21 @@ string th_x_strs[] = {
 	"+5.0E-05",
 };
 
-string f = "acceptance_test.root";
+string f = "validation_vs_old.root";
 
 //----------------------------------------------------------------------------------------------------
 
 for (string th_x_str : th_x_strs)
 {
 	NewPad("$\th_y^*\ung{\mu rad}$", "$A_{\rm sm}(\th_x^*, \th_y^*)$");
+	currentpad.xTicks = LeftTicks(0.1, 0.05);
+
 	draw(scale(1e6, 1.), RootGetObject(f, "g_old_A_th_x_" + th_x_str), blue);
 	draw(scale(1e6, 1.), RootGetObject(f, "g_new_A_th_x_" + th_x_str), red+dashed);
-	AttachLegend(th_x_str, NW, NW);
+
+	limits((4, 0), (5, 1), Crop);
+
+	AttachLegend("$\th_x^* = $" + th_x_str, NW, NW);
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -24,6 +29,12 @@ for (string th_x_str : th_x_strs)
 NewRow();
 
 NewPad("$|t|\ung{GeV^2}$", "$A_\ph(t)$");
-draw(RootGetObject(f, "g_A_t_old"), blue);
-draw(RootGetObject(f, "g_A_t_new"), red+dashed);
+draw(RootGetObject(f, "g_A_t_old"), blue, "old");
+draw(RootGetObject(f, "g_A_t_new"), red+dashed, "new");
 
+limits((0, 0), (0.5, 0.5), Crop);
+
+frame f_legend = BuildLegend();
+
+NewPad(false);
+add(f_legend);
