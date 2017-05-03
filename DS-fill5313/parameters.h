f6e94a467eeccfb258bfc356a4e1413d2a365a02
Jan Kašpar <jan.kaspar@cern.ch>
2017-05-03 19:18:49 +0200

    Normalisation updated.

--- DS-fill5313/parameters.h
+++ DS-fill5313/parameters.h
@@ -36,7 +36,7 @@ void Init_45b_56t()
 	anal.cut1_a = 1.; anal.cut1_c = 0.2E-6; anal.cut1_si = 14E-6;
 	anal.cut2_a = 1.; anal.cut2_c = 0.000E-6; anal.cut2_si = 0.38E-6;
 
-	anal.L_int = 3.4433E+04;
+	anal.L_int = 3.4429E+04;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -49,5 +49,5 @@ void Init_45t_56b()
 	anal.cut1_a = 1.; anal.cut1_c = -0.1E-6; anal.cut1_si = 14E-6;
 	anal.cut2_a = 1.; anal.cut2_c = +0.000E-6; anal.cut2_si = 0.38E-6;
 
-	anal.L_int = 3.4385E+04;
+	anal.L_int = 3.4381E+04;
 }
--- DS-fill5321/parameters.h
+++ DS-fill5321/parameters.h
@@ -36,7 +36,7 @@ void Init_45b_56t()
 	anal.cut1_a = 1.; anal.cut1_c = +0.20E-6; anal.cut1_si = 14E-6;
 	anal.cut2_a = 1.; anal.cut2_c = +0.008E-6; anal.cut2_si = 0.38E-6;
 
-	anal.L_int = 7.7930E+04;
+	anal.L_int = 7.7920E+04;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -49,5 +49,5 @@ void Init_45t_56b()
 	anal.cut1_a = 1.; anal.cut1_c = -0.1E-6; anal.cut1_si = 14E-6;
 	anal.cut2_a = 1.; anal.cut2_c = +0.010E-6; anal.cut2_si = 0.38E-6;
 
-	anal.L_int = 7.7589E+04;
+	anal.L_int = 7.7580E+04;
 }
